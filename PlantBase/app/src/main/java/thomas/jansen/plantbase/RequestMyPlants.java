36446f653b65f7dc003d56e4deb90b41bcfd7250
Thomas-Jansen <thomasjansen010@gmail.com>
2018-06-18 22:17:28 +0200

    Added pictures and functionality

---
+++ PlantBase/app/src/main/java/thomas/jansen/plantbase/RequestMyPlants.java
@@ -0,0 +1,52 @@
+package thomas.jansen.plantbase;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static thomas.jansen.plantbase.AccountActivity.mAuth;
+
+public class RequestMyPlants {
+
+    ArrayList<MyPlant> arrayListMyPlants = new ArrayList<>();
+
+    Callback callback;
+    public interface Callback {
+        void gotMyPlantsArray(ArrayList<MyPlant> arrayListMyPlants);
+        void gotError(DatabaseError error);
+    }
+
+    public void RequestMyPlants(Callback callback) {
+        this.callback = callback;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users").child(Objects.requireNonNull(mAuth.getUid()));
+        myRef.orderByChild("name").addValueEventListener(new myPlantListener());
+    }
+
+    private class myPlantListener implements ValueEventListener {
+
+
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            for (DataSnapshot child : dataSnapshot.getChildren()) {
+                MyPlant myplant = child.getValue(MyPlant.class);
+                if (myplant != null) {
+                    System.out.println(myplant.getName());
+                    arrayListMyPlants.add(myplant);
+                }
+            }
+            callback.gotMyPlantsArray(arrayListMyPlants);
+        }
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+           callback.gotError(databaseError);
+        }
+
+    }
+
+}
---
+++ PlantBase/app/src/main/java/thomas/jansen/plantbase/UpdateMyPlantActivity.java
@@ -0,0 +1,57 @@
+package thomas.jansen.plantbase;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+import static android.widget.Toast.LENGTH_LONG;
+import static thomas.jansen.plantbase.AccountActivity.mAuth;
+
+public class UpdateMyPlantActivity {
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private MyPlant myPlant;
+    private Context context;
+
+    public UpdateMyPlantActivity(MyPlant myPlant, Context context) {
+
+        if (mAuth == null) {
+            mAuth = FirebaseAuth.getInstance();
+        }
+        this.context = context;
+        this.myPlant = myPlant;
+        this.database = FirebaseDatabase.getInstance();
+        this.myRef = database.getReference("users").child(Objects.requireNonNull(mAuth.getUid()));
+        myRef.orderByChild("name").addValueEventListener(new myPlantListener());
+    }
+
+    private class myPlantListener implements ValueEventListener {
+
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            for (DataSnapshot child : dataSnapshot.getChildren()) {
+                MyPlant myplant = child.getValue(MyPlant.class);
+                if (myplant != null) {
+                    System.out.println(myplant.getName());
+                    if (myplant.getStartdate().compareTo(myPlant.getStartdate()) == 0) {
+                        myRef.child(child.getKey()).setValue(myPlant);
+                        System.out.println("geupdatet");
+                    }
+                }
+            }
+        }
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            Toast.makeText(context, (CharSequence) databaseError, LENGTH_LONG).show();
+        }
+    }
+}
